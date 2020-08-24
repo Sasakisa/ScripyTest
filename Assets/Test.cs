6dcae7ac8c843f472ff7d21584c98c0c5667ea23
Sasakisa <rr0108fp@ed.ritsumei.ac.jp>
2020-08-24 13:06:14 +0900

    ScriptTestプロジェクト　課題提出

---
+++ Assets/Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour {
+
+    void Start(){
+        
+        //課題
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < array.Length; i++){
+            Debug.Log(array[i]);
+        }
+        for (int i = array.Length-1; i >= 0; i--){
+            Debug.Log(array[i]);
+        }
+        
+
+        //発展課題
+        Boss lastboss = new Boss();
+
+        for(int i=0; i<11; i++){
+            lastboss.Magic();
+        }
+          
+    }
+
+}
+
+public class Boss{
+
+    private int mp = 53;
+    public void Magic(){
+
+        if (mp >= 5){
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        }else{
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+
+    }
+
+}
